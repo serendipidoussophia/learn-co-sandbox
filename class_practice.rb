f396ea4ee24d8122522d631ee188f7fcde0b777a
serendipidoussophia <scurranmoore@gmail.com>
2018-07-27 15:28:10 +0000

    automatically backed up by learn

--- class_practice.rb
+++ class_practice.rb
@@ -1,2 +1,21 @@
 class FacebookProfiles
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  
+  def bio = (bio)
+    @bio = bio
+  end
+  
+  def bio
+    @bio
+  end
+  
+  attr_accessor :job
+  
 end
+
+becca = FacebookProfiles.new("Becca", "23")
+becca.bio = "This is my bio!"
+becca.job = "Elementary teacher"
